79d7ebb5863953635d1815caffa2a75d36e1397d
Sage Santomenna <chellster1003@gmail.com>
2023-06-25 19:19:37 -0700

    fixed a faulty temperature calculation

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ from datetime import datetime
 from matplotlib import pyplot as plt
 from IPython.display import display
 import warnings
+
 warnings.simplefilter(action='ignore', category=FutureWarning)
 
 pd.set_option('display.max_columns', None)
@@ -22,10 +23,12 @@ earthRadius = 6371000  # m
 earthDensity = 5520  # kg m^-3
 earthEscapeVel = 11200  # m s^-1
 
+
 # Returns the density of the planet in earth densities, provided mass and radius are given in earth units
 def calcDensity(mass, radius):
     volume = (4 / 3) * pi * pow(radius * earthRadius, 3)
     return (mass * earthMass / volume) / earthDensity
+
 
 # calc escape vel provided values in earth units
 def calcEscapeVel(eMass, eRadius):
@@ -39,23 +42,33 @@ def calcSurfTemp(luminosity, semimajor, **kwargs):
     if "solarBody" in kwargs.keys() is not None:
         p = kwargs["solarBody"]
         albedo, greenhouse, antiGreenhouse = p.albedo, p.greenhouse, p.antiGreenhouse
-    #here, we grab manually provided albedo and greenhouse if no solarBody was provided
+    # here, we grab manually provided albedo and greenhouse if no solarBody was provided
     elif "albedo" in kwargs.keys() and "greenhouse" in kwargs.keys() and "antiGreenhouse" in kwargs.keys():
         albedo, greenhouse, antiGreenhouse = kwargs["albedo"], kwargs["greenhouse"], kwargs["antiGreenhouse"]
-    #uh oh
+    # uh oh
     else:
         raise Exception('Must provide a solar body or albedo, greenhouse, and antiGreenhouse')
 
-    #solar luminosity will be in units of log[solar luminosity] - we need to convert to watts
-    luminosity = pow(10,luminosity)*solarLuminosity
-    #semimajor is already in units of km, multiply by 1000 for m
-    semimajor = semimajor * 10e3
+    # solar luminosity will be in units of log[solar luminosity] - we need to convert to watts
+    # if p.name == "Earth":
+    #     print("Lum before conversion:", luminosity)
+    luminosity = 10 ** luminosity * solarLuminosity
+    # if p.name == "Earth":
+    #     print("Lum after conversion:", luminosity)
+
+    # semimajor is already in units of km, multiply by 1000 for m
+    semiMeters = semimajor * 1000
 
     effectiveTemp = pow(
-        (1 - albedo) * luminosity / (16 * pi * semimajor ** 2 * stefanBoltzmannConstant), 1 / 4)
+        (1 - albedo) * luminosity / (16 * pi * semiMeters ** 2 * stefanBoltzmannConstant), 1 / 4)
 
     surfaceTemp = pow((pow(effectiveTemp, 4) * (1 - antiGreenhouse) * (
-                1 + (3 / 4) * greenhouse) + antiGreenhouse / 2), 1 / 4)
+            1 + (3 / 4) * greenhouse) + antiGreenhouse / 2), 1 / 4)
+
+    # if p.name == "Earth":
+    #     print("semimajor (meters):", semiMeters)
+    #     print("effTemp:", effectiveTemp)
+    #     print("surfaceTemp:", surfaceTemp,"\n")
 
     # *** old way of doing it:
     # #this calculates incident stellar flux from eq 1 in mendez et al.
@@ -65,22 +78,25 @@ def calcSurfTemp(luminosity, semimajor, **kwargs):
     # # print("temp: ", temp)
     return surfaceTemp
 
-#perform the weighted product (ESI) calc
+
+# perform the weighted product (ESI) calc
 def calcESI(refVals, vals, weights):
     esi = 1
     for i, val in enumerate(vals):
         esi *= pow(1 - abs((refVals[i] - val) / (refVals[i] + val)), weights[i])
-    return esi
+    return round(esi,3)
 
-#define a class for storing earth, venus, etc
+
+# define a class for storing earth, venus, etc
 class solarBody:
-    def __init__(self, name, albedo, greenhouseConstant,antiGreenhouse):
+    def __init__(self, name, albedo, greenhouseConstant, antiGreenhouse):
         self.name = name
         self.albedo = albedo
         self.greenhouse = greenhouseConstant
         self.antiGreenhouse = antiGreenhouse
 
-#these represent our science fiction exoplanets
+
+# these represent our science fiction exoplanets
 class compBody:
     def __init__(self, name, semimajor, earthRads, escapeVel, surfaceTemp, density, exponents):
         self.semimajor = semimajor  # km
@@ -89,8 +105,8 @@ class compBody:
         self.escapeVel = escapeVel  # earth escape velocities
         self.surfTemp = surfaceTemp  # K
         self.density = density  # earth densities
-        self.exponents = exponents #pass in exponents for each compBody that determine how planets will be compared to it
-        self.refs = [self.radius, self.escapeVel, self.density, self.surfTemp, self.surfTemp, self.surfTemp, self.surfTemp] #ignore how horribly inelegant this is
+        self.exponents = exponents  # pass in exponents for each compBody that determine how planets will be compared to it
+        self.refs = [self.radius, self.escapeVel, self.density, self.surfTemp, self.surfTemp, self.surfTemp, self.surfTemp]  # ignore how horribly inelegant this is
 
 
 # provided data is as follows:
@@ -103,7 +119,7 @@ class compBody:
 
 
 def pruneData(dataFrame):
-    #drop unnecessary columns
+    # drop unnecessary columns
     # dataFrame = dataFrame.drop('eccentricity', axis=1)
     # dataFrame = dataFrame.drop('earthFlux', axis=1)
     # dataFrame = dataFrame.drop('equilTemp', axis=1)
@@ -114,10 +130,9 @@ def pruneData(dataFrame):
 
     # Removes any exoplanets if its existence is controversial
     dataFrame = dataFrame.loc[dataFrame['controversial'] == 0]
-    dataFrame = dataFrame.loc[dataFrame['defaultPlanetEntry'] == 1]  #avoid multiple entries of the same planet by only using the best entry
+    dataFrame = dataFrame.loc[dataFrame['defaultPlanetEntry'] == 1]  # avoid multiple entries of the same planet by only using the best entry
 
-    dataFrame = dataFrame[~dataFrame['solution'].str.contains('Candidate')] #candidate not in soltype gets rid of kepler and tess candidates
-
+    dataFrame = dataFrame[~dataFrame['solution'].str.contains('Candidate')]  # candidate not in soltype gets rid of kepler and tess candidates
 
     # Removes any exoplanets that don't have the required data
     dataFrame = dataFrame[dataFrame['planetSemimajor'].notna()]
@@ -126,6 +141,11 @@ def pruneData(dataFrame):
     dataFrame = dataFrame[dataFrame['stellarRadius'].notna()]
     dataFrame = dataFrame[dataFrame['stellarEffTemp'].notna()]
     dataFrame = dataFrame[dataFrame['luminosity'].notna()]
+
+    dfNa = dataFrame.loc[dataFrame["eccentricity"].isna()]
+    dfLess = dataFrame.loc[dataFrame["eccentricity"] <= 0.1]
+    dataFrame = dfNa.append(dfLess, ignore_index=True)
+
 
 
     return dataFrame
@@ -147,10 +167,12 @@ def dfCalcs(dataFrame):
     return dataFrame
 
 
-#calculate similarity to a given compbody for all exoplanets
+# calculate similarity to a given compbody for all exoplanets
 def compareToPlanet(compBody, dataframe, exponents):
     df = dataframe
-    df['similarityIndex_' + compBody.name] = df.apply(lambda row: calcESI(compBody.refs, [row['planetEarthRads'], row['escapeVelocity'], row['density'], row['surfaceEarthTemp'], row['surfaceVenusTemp'], row['surfaceMarsTemp'], row['surfaceTitanTemp']], exponents), axis=1)
+    df['similarityIndex_' + compBody.name] = df.apply(lambda row: calcESI(compBody.refs,
+                                                                          [row['planetEarthRads'], row['escapeVelocity'], row['density'], row['surfaceEarthTemp'], row['surfaceVenusTemp'],
+                                                                           row['surfaceMarsTemp'], row['surfaceTitanTemp']], exponents), axis=1)
 
 
 # Dumps the top five planets of a value 'value' and from a dataframe 'inFrame' to a csv in location 'path'
@@ -164,40 +186,48 @@ def getMatchesCSV(inFrame, value, path):
         tempDict = dataframe.loc[highestIndex].to_dict()
         outFrame = outFrame.append(tempDict, ignore_index=True)
         dataframe = dataframe.drop(highestIndex)
-    outFrame.to_csv(path)
+    outFrame.to_csv(path,index=None)
 
 
 # Creating solarBodies for references
-solarEarth = solarBody('Earth', 0.301, 0.385,0)
-solarVenus = solarBody('Venus', 0.760, 0.990,0)
-solarMars = solarBody('Mars', 0.250, 0.073,0)
-solarTitan = solarBody('Titan', 0.265, 0.338,4/7)
-solarErid = solarBody('Erid',0.014,0,0)
+solarEarth = solarBody('Earth', 0.301, 0.87, 0)
+solarVenus = solarBody('Venus', 0.760, 137, 0)
+solarMars = solarBody('Mars', 0.250, 0.12, 0)
+solarTitan = solarBody('Titan', 0.265, 3, 4 / 7)
+solarErid = solarBody('Erid', 0.014, 0, 0)
 
-#these constants will be used to define exponents
-r = 1/3
-v = 1/3
-d = 1/3
+
+#these use greenhouse constant vals instead of gray opacity:
+# solarEarth = solarBody('Earth', 0.301, 0.385, 0)
+# solarVenus = solarBody('Venus', 0.760, 0.990, 0)
+# solarMars = solarBody('Mars', 0.250, 0.073, 0)
+# solarTitan = solarBody('Titan', 0.265, 0.338, 4 / 7)
+# solarErid = solarBody('Erid', 0.014, 0, 0)
+
+# these constants will be used to define exponents
+r = 1 / 3
+v = 1 / 3
+d = 1 / 3
 x = 1
 
 # Creating planets from real life or sci fi
 # name, semimajor, rads, escape, surface temp, density
-compEarth = compBody('Earth', AU, 1, 1, 288, 1,[r,v,d,x,0,0,0])
+compEarth = compBody('Earth', AU, 1, 1, 288, 1, [r, v, d, x, 0, 0, 0])
 # compJupiter = compBody('Jupiter', 5.2038*AU, 11.2, 5.317247, 123, 0.2404)
-compArrakis = compBody('Arrakis', 2.3*AU, 0.979, calcEscapeVel(0.843, 0.979), 325, calcDensity(0.843, 0.979),[r,v,d,x,0,0,0])
-compDhrawn = compBody('Dhrawn', 0.3*AU, 8.5, calcEscapeVel(3000, 8.5), 250, calcDensity(3000, 8.5),[r,v,d,0.5*x,0,0.5*x,0])
-compErid = compBody('Erid', 0.224*AU, 2.01, calcEscapeVel(8.47, 2.01), 480, calcDensity(8.47, 2.01), [r,v,d,x,0,0,0]) # <-- placeholder exponents for erid, need to discuss
+compArrakis = compBody('Arrakis', 2.3 * AU, 0.979, calcEscapeVel(0.843, 0.979), 325, calcDensity(0.843, 0.979), [r, v, d, x, 0, 0, 0])
+compDhrawn = compBody('Dhrawn', 0.3 * AU, 8.5, calcEscapeVel(3000, 8.5), 250, calcDensity(3000, 8.5), [r, v, d, 0.5 * x, 0, 0.5 * x, 0])
+compErid = compBody('Erid', 0.224 * AU, 2.01, calcEscapeVel(8.47, 2.01), 480, calcDensity(8.47, 2.01), [r, v, d, x, 0, 0, 0])  # <-- placeholder exponents for erid, need to discuss
 # compHabranah = compBody('Habranah', 1.5*AU, 0.21, calcEscapeVel(0.01, 0.21), 195, calcDensity(0.01, 0.21))
-compHekla = compBody('Hekla', 0.3*AU, 2, calcEscapeVel(1.45, 2), 260, calcDensity(1.45, 2),[r,v,d,0,0,x,0])
-compMesklin = compBody('Mesklin', 3.3*AU, 7.85, calcEscapeVel(5086.51, 7.85), 100, calcDensity(5086.51, 7.85),[r,v,d,0,0,0,x])
-compSarr = compBody('Sarr', 1.6*AU, 0.76, calcEscapeVel(0.45, 0.76), 800, calcDensity(0.45, 0.76),[r,v,d,0,x,0,0])
-compTenebra = compBody('Tenebra', 2*AU, 3, calcEscapeVel(27, 3), 650, calcDensity(27, 3),[r,v,d,0.25*x,0.75*x,0,0])
+compHekla = compBody('Hekla', 0.3 * AU, 2, calcEscapeVel(1.45, 2), 260, calcDensity(1.45, 2), [r, v, d, 0, 0, x, 0])
+compMesklin = compBody('Mesklin', 3.3 * AU, 7.85, calcEscapeVel(5086.51, 7.85), 100, calcDensity(5086.51, 7.85), [r, v, d, 0, 0, 0, x])
+compSarr = compBody('Sarr', 1.6 * AU, 0.76, calcEscapeVel(0.45, 0.76), 800, calcDensity(0.45, 0.76), [r, v, d, 0, x, 0, 0])
+compTenebra = compBody('Tenebra', 2 * AU, 3, calcEscapeVel(27, 3), 650, calcDensity(27, 3), [r, v, d, 0.25 * x, 0.75 * x, 0, 0])
 
-#collect our solar bodies
+# collect our solar bodies
 solarBodies = [solarEarth, solarVenus, solarMars, solarTitan]
-#collect our comparison (scifi) bodies
+# collect our comparison (scifi) bodies
 compBodies = [compEarth, compArrakis, compErid, compMesklin, compDhrawn, compHekla, compSarr,
-              compTenebra] #compHabranah and compJupiter eliminated 1/11/23 by ben and sage
+              compTenebra]  # compHabranah and compJupiter eliminated 1/11/23 by ben and sage
 
 # import and manage data
 # df = pd.read_csv('exoData.csv')
@@ -205,27 +235,28 @@ df = pd.read_csv('2023.05.29_data.csv')
 df = pruneData(df)
 df = dfCalcs(df)
 
-#obviously these need tuning
+# obviously these need tuning
 
-#generate an output path, make directories
+# generate an output path, make directories
 path = 'outputs/' + str(datetime.strftime(datetime.now(), "%Y-%m-%d-%H-%M-%S")) + '/'
 os.mkdir(path)
 os.mkdir(path + '/plots')
 os.mkdir(path + '/csvs')
 
-#make a dataframe to store ESI values in
-compBodiesESI = pd.DataFrame(columns=["Name","ESI"])
+# make a dataframe to store ESI values in
+compBodiesESI = pd.DataFrame(columns=["Name", "ESI"])
 
-#calculate the esi of each of the scifi bodies
+# calculate the esi of each of the scifi bodies
 for compBody in compBodies:
     esi = calcESI(compEarth.refs, compBody.refs, compBody.exponents)
-    row = {"Name": [compBody.name], "ESI": [esi]}
+    row = {"Name": compBody.name, "ESI": esi}
     compBodiesESI = compBodiesESI.append(row, ignore_index=True)
+    compBodiesESI = compBodiesESI.round(decimals=3)
 display(compBodiesESI)
 compBodiesESI.to_csv(path + 'fictionalPlanetESI.csv')
 
-averageSimilarity = pd.DataFrame(columns=["Name","Average"])
-#do the calculations
+averageSimilarity = pd.DataFrame(columns=["Name", "Average", "Median", "Standard Deviation", "Max"])
+# do the calculations
 for body in compBodies:
     # **** for each of our sci fi planets, we: ----------------------------------
     name = body.name
@@ -234,10 +265,10 @@ for body in compBodies:
     os.mkdir(path + '/csvs/' + name)
     # **** compare all the exoplanets to it -------------------------------------
     compareToPlanet(body, df, body.exponents)
-        # stats = df['similarityIndex_' + name].describe()
-        # print(stats)
+    # stats = df['similarityIndex_' + name].describe()
+    # print(stats)
     # **** start collecting data with the "row" object --------------------------
-    row = {"Name": [body.name], "Average": [df['similarityIndex_' + name].mean()]}
+    row = {"Name": body.name, "Average": round(df['similarityIndex_' + name].mean(),3), "Median": round(df['similarityIndex_' + name].median(),3), "Standard Deviation": round(df['similarityIndex_' + name].std(),3),"Max":round(df['similarityIndex_' + name].max(),3)}
     averageSimilarity = averageSimilarity.append(row, ignore_index=True)
     # **** plot and save figs ---------------------------------------------------
     plotPath = path + '/plots/' + name + '/'
@@ -259,13 +290,26 @@ for body in compBodies:
     # plt.xlim(0, 400)
     plt.savefig(plotPath + name + 'period.png')
     plt.close()
+
+    plt.hist(x=df['similarityIndex_' + name], bins=10, rwidth=0.9)
+    plt.ylabel("Frequency")
+    plt.xlabel("Similarity to " + name)
+    plt.title(name)
+    plt.xlim(0, 1)
+    plt.savefig(plotPath + name + 'histogram.png')
+    plt.xlim(0,1)
+    plt.close()
+
+
     # **** save to a csv --------------------------------------------------------
+    df = df.round(decimals=3)
     df.to_csv(path + '/csvs/' + name + '/ref' + name + '.csv')
     # **** save top 5 matches to a csv ------------------------------------------
     getMatchesCSV(df, 'similarityIndex_' + name, path + '/csvs/' + name + '/topFive' + name + '.csv')
-df.to_csv(path + "data" + '.csv')
+df = df.round(decimals=3)
+df.to_csv(path + "data" + '.csv', index=None)
 
 # **** sort the averageSimilarity by name and then save it ----------------------
-averageSimilarity = averageSimilarity.sort_values('Name')
-averageSimilarity.to_csv(path + 'fictionalPlanetAverage.csv')
+averageSimilarity = averageSimilarity.sort_values('Name').round({"Average":3})
+averageSimilarity.to_csv(path + 'fictionalPlanetAverage.csv', index=None)
 print(averageSimilarity.to_string())
